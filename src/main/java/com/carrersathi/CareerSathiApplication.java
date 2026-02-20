6665aacd9ea83cda67a9dcd4b50e913ae9c410e4
Adityaxletscode <150706824+Adityaxletscode@users.noreply.github.com>
2026-02-21 02:11:11 +0530

    Initial commit

---
+++ src/main/java/com/carrersathi/CareerSathiApplication.java
@@ -0,0 +1,12 @@
+package com.carrersathi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerSathiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerSathiApplication.class, args);
+	}
+}
